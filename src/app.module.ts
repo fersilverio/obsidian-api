b34e091521adcaef4763365172a2ea7e318e8f1b
fersilverio <ssilveriofernando45@gmail.com>
2024-06-10 23:37:00 -0300

    auth module initialization

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AssociateUserModule } from './associate-user/associate-user.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [AssociateUserModule],
+  imports: [AssociateUserModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
