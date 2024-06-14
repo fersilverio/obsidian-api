97be3adbb3ee2d8e0ebd58f9da29d3acec0bd9a0
fersilverio <ssilveriofernando45@gmail.com>
2024-06-13 22:27:56 -0300

    ok

--- src/associate-user/repositories/__tests__/in-memory-associate-users.repository.spec.ts
+++ src/associate-user/repositories/__tests__/in-memory-associate-users.repository.spec.ts
@@ -7,7 +7,29 @@ describe("InMemoryAssociateUsersRepository unit tests", () => {
         sut = new InMemoryAssociateUsersRepository();
     });
 
-    it("Should fail when attempt to create an user without data", () => {
-        expect(sut.createUser(null)).rejects.toThrow("No data provided!");
-    });
+    describe("createUser functionality", () => {
+        it("Should fail when attempt to create an user without data", () => {
+            expect(sut.createUser(null)).rejects.toThrow("No data provided!");
+        });
+
+        it("Should create an user", async () => {
+            const user = await sut.createUser({
+                name: "test",
+                email: "test@example.com",
+                password: "1234567"
+            });
+
+            expect(user).toMatchObject({
+                id: expect.any(Number),
+                name: "test",
+                email: "test@example.com",
+                password: "1234567",
+                level: expect.any(Number),
+                rank: expect.any(Number),
+                clan: expect.any(String),
+                numberOfCards: expect.any(Number),
+                createdAt: expect.any(Date),
+            })
+        })
+    })
 })
