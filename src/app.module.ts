b0c8697c40d690f2b392368fe68c454aaf00f7e2
fersilverio <ssilveriofernando45@gmail.com>
2024-06-15 15:51:09 -0300

    removing default services - not used

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,10 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { AssociateUserModule } from './associate-user/associate-user.module';
 import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [AssociateUserModule, AuthModule],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [],
+  providers: [],
 })
-export class AppModule {}
+export class AppModule { }
